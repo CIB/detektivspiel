5c4f44566838941011fc77d6fa5d5fc124f9ef94
Christian Bielert <Christian.Bielert@msg.group>
2019-11-20 19:12:08 +0100

    Initial commit.

---
+++ client/src/components/GameScreen.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <img v-if="open" src="../assets/door_open.png" @click="clicked" />
+    <img v-else src="../assets/door_closed.png" @click="clicked" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class GameScreen extends Vue {
+  @Prop() private msg!: string;
+
+  open: boolean = true;
+
+  clicked() {
+    this.open = !this.open;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ server/app.ts
@@ -0,0 +1,14 @@
+import * as Server from "socket.io";
+const io = new Server();
+
+import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+let http = require("http").Server(app);
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http);
